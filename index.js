af3c980d5e03705b10765d9aa20dff2fc737b351
RBar70 <R.barber@utah.edu>
2021-11-23 10:11:28 -0700

    Fingers Crossed

---
+++ index.js
@@ -0,0 +1,5 @@
+var ThankgivingTime = "Thesesa is in town! Max is here too. Amelia and Zander are very happy to see them both!!!!"
+document.querySelector("#ThreseaIsInTown").textContent = ThankgivingTime
+
+var ThankgivingTime2 = "This was my first time using Java Script, HTML & CSS together!!!!"
+document.querySelector("#ThreseaIsInTown2").textContent = ThankgivingTime2
